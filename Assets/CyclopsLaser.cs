d85406f72501105758fa64774010259419a0dc89
gamesketches <sam.von.ehren@gmail.com>
2016-02-20 15:53:02 -0500

    Made a button, you can look at it to open a door

---
+++ Assets/CyclopsLaser.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CyclopsLaser : MonoBehaviour {
+
+	public GameObject targetDoor;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray PsychicRay = new Ray(transform.position, transform.forward);
+
+		if(Physics.Raycast(PsychicRay, out hit, 40f)){
+			if(hit.collider.tag == "button"){
+				targetDoor.GetComponentInChildren<UnlockDoor>().OpenSesame();
+			}
+		}
+		Debug.DrawRay(transform.position, transform.forward * 100f);
+	}
+}
---
+++ Assets/UnlockDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnlockDoor : MonoBehaviour {
+
+	public float totalMovement;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void OpenSesame() {
+		StartCoroutine(open());
+	}
+
+	IEnumerator open() {
+		Vector3 target = transform.position + new Vector3(0.0f, totalMovement, 0.0f);
+		while(transform.position.y <= target.y) {
+			transform.position = new Vector3(transform.position.x, transform.position.y + (1.0f * Time.deltaTime), transform.position.z);
+			yield return null;
+		}
+	}
+}
