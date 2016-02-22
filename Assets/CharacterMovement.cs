486b4e08e14c9fc5cbb4c4187bd9e1816cc55381
gamesketches <sam.von.ehren@gmail.com>
2016-02-21 22:06:24 -0500

    Added in switching eye balls on and off with a canvas.

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class CharacterMovement : MonoBehaviour {
 
 	public GameObject rightEye;
+	public GameObject leftEye;
+	public Canvas rightBlinder;
+	public Canvas leftBlinder;
 	CharacterController controller;
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController>();
+		rightBlinder.enabled = false;
+		leftBlinder.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -17,11 +22,18 @@ public class CharacterMovement : MonoBehaviour {
 
 		controller.Move(transform.forward * vert + Physics.gravity);
 		transform.Rotate(0f, hori * 2f, 0f);
+
+		if(Input.GetKeyDown(KeyCode.Space) == true && rightEye.transform.parent == null) {
+			leftBlinder.enabled = !leftBlinder.enabled;
+			rightBlinder.enabled = !rightBlinder.enabled;
+
+		}
 	}
 
 	public void dropEye(Vector3 targetPos) {
 		rightEye.transform.position = targetPos;
 		rightEye.transform.rotation = Quaternion.Euler(0, 270, 0);
 		rightEye.transform.parent = null;
+		rightBlinder.enabled = true;
 	}
 }
