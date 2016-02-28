b433bd580add49ed9f0f222ed697e2a3a4ec0499
gamesketches <sam.von.ehren@gmail.com>
2016-02-28 12:24:17 -0500

    Removed legacy code

--- Assets/EyeballMovement.cs
+++ Assets/EyeballMovement.cs
@@ -8,7 +8,6 @@ public class EyeballMovement : MonoBehaviour {
 	public float eyeDropSpeed;
 	private Canvas rightBlinder;
 	delegate void SocketFunction();
-	private SocketFunction socketingFunction;
 	// Use this for initialization
 	void Start () {
 		conveyorBelt = false;
@@ -23,14 +22,6 @@ public class EyeballMovement : MonoBehaviour {
 			Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - (1 * Time.deltaTime));
 			transform.position = newPos;
 		}
-	}
-
-	public void dropEye(Vector3 targetPos) {
-		StartCoroutine(eyeMovement(targetPos, leaveSocket));
-	}
-
-	public void pickupEye(Vector3 targetPos) {
-		StartCoroutine(eyeMovement(targetPos, returnToSocket));
 	}
 
 	public IEnumerator detachEye(Vector3 targetPos) {
@@ -58,29 +49,6 @@ public class EyeballMovement : MonoBehaviour {
 			t += Time.deltaTime/eyeDropSpeed;
 			yield return null;
 		}
-	}
-
-	IEnumerator eyeMovement(Vector3 targetPos, SocketFunction socketFunction) {
-		Vector3 startPos = transform.position;
-		float t = 0f;
-		while(t <= 1.0f) {
-			transform.position = Vector3.Lerp(startPos, targetPos, t);
-			t += Time.deltaTime/eyeDropSpeed;
-			yield return null;
-		}
-		socketFunction();
-	}
-
-	void leaveSocket() {
-		transform.rotation = Quaternion.Euler(0, 270, 0);
-		transform.parent = null;
-		conveyorBelt = true;
-		rightBlinder.enabled = true;
-	}
-
-	void returnToSocket() {
-		transform.localRotation = Quaternion.Euler(Vector3.forward);
-		rightBlinder.enabled = false;
 	}
 
 }
