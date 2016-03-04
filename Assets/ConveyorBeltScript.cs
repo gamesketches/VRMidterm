5d5ae72ab6fe157f98feb17e1ccc1255fbbe7716
gamesketches <sam.von.ehren@gmail.com>
2016-03-04 17:59:23 -0500

    Added a texture to the conveyor belts

---
+++ Assets/ConveyorBeltScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBeltScript : MonoBehaviour {
+
+	private Renderer renderer;
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTextureOffset = new Vector2(0f, Time.time * -0.01f);
+	}
+}
