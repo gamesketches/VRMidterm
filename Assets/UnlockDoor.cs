75d21e0a433ea7d9c0ccd6f18d924ee8269f577b
gamesketches <sam.von.ehren@gmail.com>
2016-03-06 18:54:48 -0500

    Added particle system to doors opening heh

--- Assets/UnlockDoor.cs
+++ Assets/UnlockDoor.cs
@@ -18,11 +18,15 @@ public class UnlockDoor : MonoBehaviour {
 	}
 
 	IEnumerator open() {
+		GameObject opening = (GameObject)Instantiate(Resources.Load("DoorOpen"));
+		opening.transform.position = new Vector3(gameObject.transform.position.x,
+				gameObject.transform.position.y, gameObject.transform.position.z);
 		Vector3 target = transform.position + new Vector3(0.0f, totalMovement, 0.0f);
 		while(transform.position.y <= target.y) {
 			transform.position = new Vector3(transform.position.x, transform.position.y + (1.0f * Time.deltaTime), transform.position.z);
 			yield return null;
 		}
+		Destroy(opening);
 	}
 
 	IEnumerator close() {
