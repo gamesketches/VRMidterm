5043eab9aa202f7b2a2c07a539119ea5bad45b2c
gamesketches <sam.von.ehren@gmail.com>
2016-03-07 12:25:25 -0500

    Fixed bug with door particles firing forever

--- Assets/UnlockDoor.cs
+++ Assets/UnlockDoor.cs
@@ -4,20 +4,26 @@ using System.Collections;
 public class UnlockDoor : MonoBehaviour {
 
 	public float totalMovement;
+	private bool mutex;
 	// Use this for initialization
 	void Start () {
-	
+		mutex = false;
 	}
 	
 	public void OpenSesame() {
-		StartCoroutine(open());
+		if(!mutex) {
+			StartCoroutine(open());
+		}
 	}
 
 	public void CloseSesame(){
-		StartCoroutine(close());
+		if(!mutex) {
+			StartCoroutine(close());
+		}
 	}
 
 	IEnumerator open() {
+		mutex = true;
 		GameObject opening = (GameObject)Instantiate(Resources.Load("DoorOpen"));
 		opening.transform.position = new Vector3(gameObject.transform.position.x,
 				gameObject.transform.position.y, gameObject.transform.position.z);
@@ -26,14 +32,17 @@ public class UnlockDoor : MonoBehaviour {
 			transform.position = new Vector3(transform.position.x, transform.position.y + (1.0f * Time.deltaTime), transform.position.z);
 			yield return null;
 		}
+		mutex = false;
 		Destroy(opening);
 	}
 
 	IEnumerator close() {
+		mutex = true;
 		Vector3 target = transform.position + new Vector3(0.0f, -totalMovement, 0.0f);
 		while(transform.position.y >= target.y) {
 			transform.position = new Vector3(transform.position.x, transform.position.y - (1.0f * Time.deltaTime), transform.position.z);
 			yield return null;
 		}
+		mutex = false;
 	}
 }
